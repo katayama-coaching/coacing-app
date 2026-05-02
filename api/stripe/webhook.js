88fd64e0dc7726f8c91725a390aecc23cecc0c75
katayama-coaching <freakers.s.k@gmail.com>
2026-05-02 17:31:42 +0900

    Create webhook.js

---
+++ api/stripe/webhook.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const sig = req.headers['stripe-signature'];
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+  } catch (err) {
+    return res.status(400).json({ error: `Webhook Error: ${err.message}` });
+  }
+
+  switch (event.type) {
+    case 'checkout.session.completed':
+      console.log('Payment successful:', event.data.object);
+      break;
+    case 'customer.subscription.deleted':
+      console.log('Subscription cancelled:', event.data.object);
+      break;
+    default:
+      console.log(`Unhandled event type: ${event.type}`);
+  }
+
+  res.status(200).json({ received: true });
+};
